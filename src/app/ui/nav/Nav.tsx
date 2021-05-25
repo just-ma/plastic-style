394d1ba0bfdd598110a5f05e0fc773f975844685
baolinhkn <baolinh.nguyen5@gmail.com>
2021-05-24 23:32:01 -0700

    hover nav

--- src/app/ui/nav/Nav.tsx
+++ src/app/ui/nav/Nav.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './Nav.module.scss';
 import NavItem from './NavItem';
 
 const NAV_MENU = [
+  { title: 'Home', link: '/' },
   {
     title: 'Reviews',
     link: '/reviews',
@@ -18,12 +19,47 @@ const NAV_MENU = [
   },
 ];
 
+const NAV_ITEM_HEIGHT = 25;
+
 export default function Nav() {
+  const currentPagePath = window.location.pathname;
+
+  let activeIndex = 0;
+  NAV_MENU.forEach((item, index) => {
+    if (currentPagePath === item.link) {
+      activeIndex = index;
+    }
+  });
+
+  const [hovered, setHovered] = useState(activeIndex);
+
+  const isActive = (link: string): boolean => {
+    const currentPagePath = window.location.pathname;
+
+    if (link === '/' && currentPagePath !== '/') {
+      return false;
+    }
+
+    if (currentPagePath.includes(link)) {
+      return true;
+    }
+
+    return false;
+  };
+
   return (
     <div className={styles.Nav}>
       <div className={styles.navItems}>
+        <div className={styles.navArrow} style={{ top: hovered * NAV_ITEM_HEIGHT }}>{`>>`}</div>
         {NAV_MENU.map((item, index) => (
-          <NavItem key={index} {...item} />
+          <NavItem
+            onMouseEnter={() => setHovered(index)}
+            onMouseLeave={() => setHovered(activeIndex)}
+            isActive={isActive(item.link)}
+            index={index}
+            key={index}
+            {...item}
+          />
         ))}
       </div>
     </div>
