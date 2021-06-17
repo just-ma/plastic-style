0c53ce52253cb373c912619d1b6ead30f988306c
Justin Ma <justin.ma@verkada.com>
2021-06-16 21:02:18 -0700

    amplify setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
 
+import config from './aws-exports';
 import App from './App';
 
 import './index.scss';
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
