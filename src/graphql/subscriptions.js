fa391e8c6135369e79e3d3fb5eda6b9309515c21
Justin Ma <justin.ma@verkada.com>
2021-06-16 21:21:49 -0700

    reviews graphql setup

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateReview = /* GraphQL */ `
+  subscription OnCreateReview {
+    onCreateReview {
+      id
+      artist
+      title
+      author
+      src
+      content
+      createdAt
+      modifiedAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateReview = /* GraphQL */ `
+  subscription OnUpdateReview {
+    onUpdateReview {
+      id
+      artist
+      title
+      author
+      src
+      content
+      createdAt
+      modifiedAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteReview = /* GraphQL */ `
+  subscription OnDeleteReview {
+    onDeleteReview {
+      id
+      artist
+      title
+      author
+      src
+      content
+      createdAt
+      modifiedAt
+      updatedAt
+    }
+  }
+`;
