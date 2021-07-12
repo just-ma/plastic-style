b979ed5cd5417cfe064f4ef504a777cefeede756
Justin Ma <justin.ma@verkada.com>
2021-07-11 22:06:10 -0700

    add reviews data

--- src/reviews/models/constants.ts
+++ src/reviews/models/constants.ts
@@ -2,7 +2,7 @@ import { Review } from './types';
 
 export const MOCK_REVIEWS: ReadonlyArray<Review> = [
   {
-    id: 'ftbc',
+    id: 'faftbc',
     title: 'Fetch the Bolt Cutters',
     artist: 'Fiona Apple',
     author: 'Parker Albert',
@@ -33,43 +33,80 @@ Apple ends the album with “On I Go,” a relentless march that vocalizes how t
 Amidst the percussive clutter of Apple’s mind, she’s able to find something that perhaps eluded her previously: clarity. She no longer needs the lush orchestration of <i>When the Pawn…</i> or the poetic complexity of <i>The Idler Wheel</i>. All she needs is a pair of bolt cutters.`,
   },
   {
-    id: '456',
-    title: 'Lumberjack',
-    artist: 'Tyler, the Creator',
-    author: 'Justin Ma',
-    createdAt: 1624082838,
-    recordLabel: 'Columbia - 2021',
-    src: 'https://media.pitchfork.com/photos/60ccbe4606280ce20ecfc149/1:1/w_160/Tyler-the-Creator-Lumberjack.jpeg',
-    content: `Add it to the canon: “Yonkers,” “Domo23,” “Who Dat Boy,” “Okra,” “Potato Salad,” and now “Lumberjack,” the songs where Tyler, the Creator is really rapping. Throw in “French,” “Rella,” and “Deathcamp,” and you’ll see Tyler does this before all of his albums, pulling together some of his best verses as if to say, Ignore the antics, the fashion lines, and the sun-soaked synths, you care because this guy can rap his head off.
-  
-“Lumberjack” delights in being self-reflexive; he’s still trying as hard as ever, while almost smirking at himself for trying at all. The sinister Tyler-produced beat is built on a sample of Gravediggaz’s “2 Cups of Blood,” and, before you know it, DJ Drama is shouting and dropping Gangsta Grillz tags. Tyler even invites Odd Future mainstays Domo Genesis, L-Boy, and Taco just to lose their minds in the background about how hard Tyler’s going, like it’s “What the Fuck Right Now.”
-  
-“Lumberjack” also follows “Who Dat Boy” as a song that embraces and amplifies rap’s mean-mugging tropes. Tyler murmurs, “Rolls-Royce pull up, Black boy hop out,” and, “Call me Lumberjack ’cause I wish a n---a would,” recurring lines that transcend with confidence and conviction. He strips the genre to its core elements: a spare minor-key beat, an understated delivery, and unfussy, inarguably tough bars.`,
+    id: 'dhwm',
+    title: 'Have We Met',
+    artist: 'Destroyer',
+    author: 'Parker Albert',
+    createdAt: 1582444800,
+    src: 'https://upload.wikimedia.org/wikipedia/en/5/56/Destroyer_-_Have_We_Met.png',
+    content: `Why is Dan Bejar happy?
+
+In psychology there is a phenomenon known as the U-Bend. It describes the steep decline of happiness starting at age 20, and the relentless climb back up beginning at age 46. The second half of the bend puzzles academics. Why, they ask, do smiles grow wider as death looms nearer? The answer may lie in the new Destroyer album.
+
+On <i>Have We Met</i>, death stalks Bejar at every turn. On the ominous track “Kinda Dark,” which is much more disturbing than its title suggests, Bejar seems trapped by the night. Bejar plants the listener in a claustrophobic soundscape riddled with frantic heartbeats, where there’s barely any air to breathe. Bejar alludes to the presence of the Boston Strangler, who’s simply sitting there, as if he’s waiting for the song to do his work for him. A bristling guitar eventually penetrates the darkness, desperately trying to come up for air before it’s pulled back under.
+
+The brilliant interplay between the instrumentation and lyrics of <i>Have We Met</i> is the key to navigating this labyrinth. “The Raven” lies at the center of the maze, an impossibly catchy track filled with endless hooks and grotesque images. A warm guitar note and a shudder of static usher in Bejar’s warning to “Just look at the world around you/ Actually no don’t look.” The music follows suit: as Bejar smoothly coos about The Grand Ole Opry of Death, an energetic shaker carries along a joyful riff that sounds blissfully ignorant of the chaos surrounding it.  Bejar preaches a similar sentiment of detachment in the balmy ballad “The Man in Black’s Blues” (“When you’re looking for nothing/And you find/Nothing is more beautiful/Than anything you ever knew”). 
+
+Bejar certainly acts oblivious to the intruding stare of the Boston Strangler, or the fragmented caws of trumpet that hover like vultures around the fringes of restless track “It Just Doesn’t Happen”. Yet the album’s closer “foolssong” confronts the realness of death as time runs out. On top of a delicate synth line, Bejar laments his dying love by painting the most beautiful and complete image on the album (“And with what shall I fix?/The hole in the bucket, dear Liza/Where the honeyed diamonds of the light leaving your eyes were?”). It is a question with no answer, but Bejar seems to find solace through asking alone. This stoic mood yields to Collins’ childlike marimbas and Bejar’s nursery-rhyme melody that give a smirking shrug to the brink of oblivion (“It ain’t easy being a baby like me/ It ain’t easy being a baby like you”). As the fading synth takes its dying breath, ghostly vocals pulled from the Destroyer archive warp into a swarm of ravenous locusts and devour everything. 
+
+Dan Bejar is 47 years old. If he follows the normal trend, he’s just beginning to escape the depressing slump of middle age. “I thought I was sick and dying,” he muses in the celebratory ”Crimson Tide”. <i>Have We Met</i> confronts the inevitability of death, but Bejar seems more energized than ever to relish his remaining time.`,
   },
   {
-    id: '789',
-    title: 'All My Heroes Are Cornballs',
-    artist: 'JPEGMafia',
-    author: 'Justin Ma',
-    createdAt: 1624082838,
-    src: 'https://upload.wikimedia.org/wikipedia/en/2/21/Jpegmafia_AMHAC_1.jpg',
-    content: `All My Heroes Are Cornballs is the third studio album by American rapper JPEGMafia, released on September 13, 2019 by EQT Recordings. After the release of his record Veteran (2018), he began recording tracks for its follow-up, with a total of around 93 songs by 2019. He handled the production, mixing and mastering in his home studio. The album features guest appearances from Abdu Ali, Helena Deland and Buzzy Lee, as well as additional vocals by Refined Sugar, Vegyn and Young Emoji. Vegyn also serves as a co-producer on one track.
-  
-An avant-garde experimental hip hop and punk rap album, All My Heroes Are Cornballs draws influences from experimental pop, glitch hop, ambient, noise and industrial music. It has a smoother and more melodic sound than its predecessor, employing uncommon song structures, extensive sampling, and a variety of vocal techniques such as rapping, screaming and singing, often in falsetto. Thematically, the album is personal, introspective, and presented in a stream of consciousness form, touching on the Internet culture, prejudice, political issues and JPEGMafia's newfound fame.
-  
-All My Heroes Are Cornballs was promoted as a "disappointment" with videos released on the rapper's YouTube, along with two singles: "Jesus Forgive Me, I Am a Thot" and "Beta Male Strategies". It was supported by the JPEGMafia Type Tour, which began in North America on October 14, 2019 and ended in Europe on July 6, 2020. Upon release, All My Heroes Are Cornballs received widespread acclaim from critics, with many highlighting the humorous, sarcastic lyrical content, as well as praising its detailed and chaotic production; some of them deemed it superior to Veteran, and it was placed in numerous publications' year-end lists. The album debuted at number 105 on the Billboard 200, becoming JPEGMafia's first entry in the chart.`,
+    id: 'gdlap',
+    title: 'A review of the LA Philharmonic conducted by Gustavo Dudamel',
+    author: 'Parker Albert',
+    createdAt: 1583222400,
+    src: 'https://csq.com/wp-content/uploads/2015/10/Gustavo-Dudamel-Opening-Image.png',
+    content: `Classical music transports you to fantastical lands, dropping you in the middle of a raging war or on top of a towering castle. These fabricated imaginations of the mind brim with emotion, transforming trivial troubles into matters of inconceivable importance. In the classical world my Uber was my chariot, dodging the spears of LA traffic as it arrived at its destination just on time.
+
+But, alas, I do not live in my fantasies and reality was much more disappointing.
+
+As I prepared to call an Uber to the Walt Disney Concert Hall, the app’s tiny text detailing the dropoff time filled me with despair. Like so many times before, I had vastly underestimated the unfathomable power of LA traffic to triple travel time. I entered the backseat with my head hung low, my friend trying to console me with the fact that we were only going to arrive five minutes late, surely they would let us in and we would be fine! But countless trips to see my mother play viola in the local orchestra had taught me otherwise: if a performance began, they would not let you in until the first piece had ended. Despite knowing this, I still sprinted out of the Uber into the venue, only to be met with a telling look on the usher’s face that crushed my last bit of hope. 
+
+The gentle and carefree music of Charles Ives was mocking me from the lobby speakers as I searched for a place to watch the remaining performance. After locating a small monitor hanging from the ceiling near the restrooms, I defeatingly slumped on a sofa and watched Gustavo Dudamel conduct the miniature figures of the orchestra. The cheerful nature of the first piece (Ives I) was beginning to shift as the strings held tension-building chords over dark accents of the low brass. This melodramatic ending of the first movement perfectly matched the anguish I felt over experiencing the piece secondhand. The second movement began with a subdued sigh, which was slightly tarnished by the ambient drone of the water fountain nearby. It gradually painted the picture of a trapped princess wistfully gazing out of a castle window, once again aligning with my desire to burst through the forbidden doors of the concert hall mid-performance. 
+
+I reluctantly resisted this urge throughout the remainder of the piece, waiting to take my seat until people filed out for intermission. I was itching to see the orchestra that had been hiding behind a Panasonic TV for the last 45 minutes, and I could feel myself relax as I finally walked into the concert hall. Inside were the lingering audience members with bladders of steel, as well as a few of the musicians preparing for battle. A cellist silently shadowing his part was soon joined by two clarinetists who practiced a line for 15 seconds before looking at each other with satisfied smiles. Eventually, the rest of the orchestra took their seats and Dudamel cued the slow timpani swell that started the piece. I could immediately tell that Dvorak 7 was a completely different beast than Ives I. The first movement consisted of frolicking woodwinds followed by a heated fray between the strings and brass. These sections alternated back and forth until the low brass led a triumphant charge to end the movement. 
+
+After the brief respite of the second movement, Dudamel began the third movement with a taut smirk. He knew something the audience didn’t. The last two movements were, indeed, full of surprises. At one moment Dudamel gently bent his knees and reached out a welcoming hand to the peacefully trilling woodwinds. The next moment he snapped to a stiff posture, stabbing the air with his baton as he whipped the strings into a frenzy. Many of these climactic moments convinced me the piece was going to end, only for the music to calm down again like a child falling asleep after a temper tantrum.
+
+The best surprise lay at the end of the fourth and final movement. The low brass took one last stand before assimilating into the swarm of strings that carried the piece into an exciting build. This found its release in a chilling french horn rip that caused a kid (perhaps the only kid present) to cover his mouth in awe. I couldn’t help but smile as the entire orchestra gave a few adrenaline-pumping punches to end the piece. The instant Dudamel lowered his hands the audience erupted into an unbelievably loud roar for such a polite crowd.
+
+The unexpected (and fantastic) end to the concert left me feeling energized and grateful even to see half of the concert. My night with the LA Philharmonic had taken me on a journey from dismay to delight, and I was glad to have taken it.`,
   },
   {
-    id: '0123',
-    title: 'Pushing the Envelope',
-    artist: 'afafafafafafafaf',
-    author: 'Justin Ma',
-    createdAt: 1624082838,
-    src: 'https://www.amatruda.eu/wp-content/uploads/2017/05/PHO_0618-12x18-Amalfi-envelope-1626x1080.jpg',
-    content: `Add it to the canon: “Yonkers,” “Domo23,” “Who Dat Boy,” “Okra,” “Potato Salad,” and now “Lumberjack,” the songs where Tyler, the Creator is really rapping. Throw in “French,” “Rella,” and “Deathcamp,” and you’ll see Tyler does this before all of his albums, pulling together some of his best verses as if to say, Ignore the antics, the fashion lines, and the sun-soaked synths, you care because this guy can rap his head off.
-  
-“Lumberjack” delights in being self-reflexive; he’s still trying as hard as ever, while almost smirking at himself for trying at all. The sinister Tyler-produced beat is built on a sample of Gravediggaz’s “2 Cups of Blood,” and, before you know it, DJ Drama is shouting and dropping Gangsta Grillz tags. Tyler even invites Odd Future mainstays Domo Genesis, L-Boy, and Taco just to lose their minds in the background about how hard Tyler’s going, like it’s “What the Fuck Right Now.”
-  
-“Lumberjack” also follows “Who Dat Boy” as a song that embraces and amplifies rap’s mean-mugging tropes. Tyler murmurs, “Rolls-Royce pull up, Black boy hop out,” and, “Call me Lumberjack ’cause I wish a n---a would,” recurring lines that transcend with confidence and conviction. He strips the genre to its core elements: a spare minor-key beat, an understated delivery, and unfussy, inarguably tough bars.`,
+    id: 'ptr',
+    title: 'A show review of Pinegrove at The Roxy',
+    author: 'Parker Albert',
+    createdAt: 1581408000,
+    src: 'https://static01.nyt.com/images/2020/01/30/arts/23pinegrove/23pinegrove-superJumbo.jpg?quality=90&auto=webp',
+    content: `Pinegrove isn’t what they used to be, and neither am I.
+
+The summer of 2017 I was reeling from heartbreak in the heat of Tennessee. My car became my sanctuary; Pinegrove became my preacher. I drove through hills and trees, Cardinal baptizing me in alt-country goodness. Maybe I wasn’t the only person in the world with these feelings? 
+
+That July I took a pilgrimage to see Pinegrove perform in a stained-glass church in Kentucky. Despite not talking to anyone and being the oldest person in the room, I was drawn into the intimate community fostered by Evan Stephens Hall’s confessional shouts. 
+
+Summer became fall, and the season of change brought a hurricane to Pinegrove’s front door. On November 21st, Hall admitted through an incredibly vague Facebook post that he had been accused of sexual coercion, and, at the accuser’s request, would be taking a year-long break from touring to attend therapy. This left me, as well as many other fans, confused as to what the alleged victim defined as “sexual coercion,” and without hearing their side of the story, I became disillusioned with the band. The neighborly nature of their songs was tarnished by the possibility that there was a more sinister side to the writer of their compassionate lyrics.
+
+A 2018 Pitchfork article shed light on a still blurry situation, giving Hall a chance to clarify the intentions of his original statement and exposing deceptive behavior of the mediator between himself and the alleged victim. The accuser did not express their side of the narrative until the beginning of 2020. She told The New Yorker that, as a member of the band’s crew on tour, there was an unhealthy power dynamic at play during her and Hall’s relationship. She also acknowledged that Hall had respected her request to take a year off touring. The context of the accuser’s experience and Hall’s compliance with her wishes reconciled most of the reservations I held about listening to Pinegrove. 
+
+Four days after The New Yorker’s piece, Pinegrove released their fourth LP Marigold. 
+
+The new album trades relatable narratives and heartfelt sincerity with bland imagery and insular platitudes. Sonically, it sinks into a slow, somber twang that dulls over time, closing with a six minute ambient dirge. Despite my feelings about Marigold, a longing for the transcendent nature of live Cardinal songs convinced me to attend Pinegrove’s show at The Roxy. 
+
+When I entered the venue I was struck by the age of most audience members. Unlike the concert I attended almost three years ago, most people were in their late twenties or early thirties. In retrospect this makes sense considering the mellowed-out mood of Marigold. 
+
+LAKE opened the show with largely inoffensive and enjoyable indie pop. Their dreamy guitar riffs and vocal melodies nonetheless sounded too safe; I wanted them to leave their comfort zone.
+
+After a respectably short break between sets, an ambient track heralded the entrance of Evan Stephens Hall and his bandmates. They opened with their biggest-hit “Old Friends,” delivering catharsis with each climactic chorus. It was fascinating to watch Hall’s face contort into emotive shapes as his drawls morphed into shouts. A large chunk of the crowd was singing along, but the frenetic energy that characterized the Kentucky show was missing. They restrained their dancing to head bobs, and sang like it was a polite gesture rather than a need for release. 
+
+After “Old Friends” ended, Hall announced they were about to play a bunch of tracks from Marigold. This included one of the worst songs off the album, “No Drugs.” The song centers around Hall sulkily saying he wants to feel good, which leads him to proclaim, “No drugs and alcohol today.” Pinegrove followed these songs with one of my favorite tracks off their third LP Skylight, “Rings.” Thankfully they brought a slide guitar along so I could hear it swell underneath Hall’s closed eyed yelp.
+
+The band entered a mid-show slump that only found respite in “Angelina,” a set staple whose acoustic version may never see the light of day again, considering Hall’s statement that he wrote the Marigold song “Phase” to immediately follow it. 
+
+After “Phase,” Pinegrove hit their stride, playing bangers back to back that comprised the best part of their show. Meridian gem “Recycling” preceded the classic Cardinal odyssey “Aphasia,” which was followed by two of the best songs off Skylight and my favorite track off Marigold, “Dotted Line.”
+
+As Hall was saying his last goodbyes before the final song, the audience erupted into applause, prompting an embarrassed laugh from the frontman. Then he launched into “Visiting,” and the track’s relentless momentum paired with Hall’s unbridled howl carried me back to that summer in Tennessee, when Cardinal was my shelter. I found myself singing along like I had done so many times before during long solitary drives through the countryside. This nostalgic bliss ended way too soon and the band exited the stage, leaving me wanting more.
+
+Even though I was disappointed that Pinegrove only played three tracks from Cardinal, I don’t blame Hall for not wanting to look back. I just wish his journey since that transformative album was filled with more surprises.`,
   },
 ];
