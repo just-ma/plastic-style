48bc0eebc3fb45cdd2a3e902f11cd2c5f09e777a
Justin Ma <justinma3@gmail.com>
2022-01-09 12:26:33 -0800

    add support for links/expandable text (#25)
    * add links
    
    * add expandable text

--- src/admin/ui/TextEditor.tsx
+++ src/admin/ui/TextEditor.tsx
@@ -1,7 +1,17 @@
 import React, { useCallback, useState, useEffect, useRef, LegacyRef, useMemo } from 'react';
 import styled from 'styled-components';
-import { ContentState, DraftHandleValue, Editor, EditorState, RichUtils } from 'draft-js';
+import {
+  ContentState,
+  DraftHandleValue,
+  Editor,
+  EditorState,
+  RichUtils,
+  CompositeDecorator,
+  ContentBlock,
+} from 'draft-js';
 import { Options, stateToHTML } from 'draft-js-export-html';
+
+import HTMLString from '../../common/ui/HTMLString';
 
 const HTMLOptions: Options = {
   defaultBlockTag: 'span',
@@ -11,10 +21,9 @@ const HTMLOptions: Options = {
 const formatHTML = (content: ContentState): string => {
   const html = stateToHTML(content, HTMLOptions)
     .replace(/<span>|<br>/g, '')
-    .replace(/<\/span>\s*/g, '\\n')
-    .slice(1);
+    .replace(/<\/span>\s*/g, '\\n');
 
-  return `\`${html.slice(0, html.length - 2)}\``;
+  return html.slice(0, html.length - 2);
 };
 
 const Page = styled.div`
@@ -40,17 +49,57 @@ const ItalicButton = styled.button`
   font-style: italic;
 `;
 
-const EditorContainer = styled.div`
+const TextContainer = styled.div<{ editable?: boolean }>`
   padding: 10px;
-  border: 1px solid black;
-  font-size: 14px;
-  line-height: 18px;
   margin-bottom: 20px;
+  min-height: 20px;
+  border-radius: 5px;
+  ${({ editable }) => (editable ? 'border: 1px solid black' : 'background-color: #d9d9d9')};
 `;
 
 const HTMLContainer = styled.div`
   user-select: all;
 `;
+
+const LinkContainer = styled.a`
+  color: blue;
+`;
+
+type LinkProps = {
+  entityKey: string;
+  contentState: ContentState;
+  children: React.ReactNode;
+};
+
+const Link = ({ entityKey, contentState, children }: LinkProps): React.ReactElement => {
+  const { url } = contentState.getEntity(entityKey).getData();
+
+  return (
+    <LinkContainer href={url} target="_blank" rel="noreferrer">
+      {children}
+    </LinkContainer>
+  );
+};
+
+const findLinkEntities = (
+  contentBlock: ContentBlock,
+  callback: (start: number, end: number) => void,
+  contentState: ContentState,
+): void => {
+  contentBlock.findEntityRanges((character) => {
+    const entityKey = character.getEntity();
+
+    return entityKey !== null && contentState.getEntity(entityKey).getType() === 'LINK';
+  }, callback);
+};
+
+export const createLinkDecorator = (): CompositeDecorator =>
+  new CompositeDecorator([
+    {
+      strategy: findLinkEntities,
+      component: Link,
+    },
+  ]);
 
 export default function TextEditor(): React.ReactElement {
   const ref = useRef<Editor>();
@@ -90,21 +139,55 @@ export default function TextEditor(): React.ReactElement {
     handleFocus();
   }, [handleFocus]);
 
+  const handleLinkClick = useCallback((): void => {
+    const selection = editorState.getSelection();
+    if (selection.getStartOffset() === selection.getEndOffset()) {
+      window.alert('make a selection first');
+      return;
+    }
+
+    const linkUrl = window.prompt('Add link (https://www.example.com)');
+    const underlinedState = RichUtils.toggleInlineStyle(editorState, 'UNDERLINE');
+    const contentStateWithEntity = editorState
+      .getCurrentContent()
+      .createEntity('LINK', 'MUTABLE', { url: linkUrl, target: '_blank', rel: 'noreferrer' });
+
+    setEditorState(
+      RichUtils.toggleLink(
+        underlinedState,
+        underlinedState.getSelection(),
+        contentStateWithEntity.getLastCreatedEntityKey(),
+      ),
+    );
+    handleFocus();
+  }, [handleFocus, editorState]);
+
   return (
     <Page>
+      editor:
       <ButtonsContainer>
         <BoldButton onClick={handleBoldClick}>B</BoldButton>
         <ItalicButton onClick={handleItalicClick}>I</ItalicButton>
+        <button onClick={handleLinkClick}>LINK</button>
       </ButtonsContainer>
-      <EditorContainer>
+      <TextContainer editable>
         <Editor
           ref={ref as LegacyRef<Editor>}
           editorState={editorState}
           handleKeyCommand={handleKeyCommand}
           onChange={setEditorState}
         />
-      </EditorContainer>
-      <HTMLContainer>{html}</HTMLContainer>
+      </TextContainer>
+      <br />
+      code to copy:
+      <TextContainer>
+        <HTMLContainer>`{html}`</HTMLContainer>
+      </TextContainer>
+      <br />
+      website text display:
+      <TextContainer>
+        <HTMLString>{html}</HTMLString>
+      </TextContainer>
     </Page>
   );
 }
