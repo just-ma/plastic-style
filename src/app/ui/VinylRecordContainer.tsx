8a1c2a86ed6b3f376fb7959b459f21b4486ca783
Justin Ma <justin.ma@verkada.com>
2023-03-13 12:45:29 -0700

    chris brokaw

--- src/app/ui/VinylRecordContainer.tsx
+++ src/app/ui/VinylRecordContainer.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import useResponsive from '../../common/hooks/useResponsive';
 import useIsHome from '../../home/hooks/useIsHome';
 
-import AiGif from '../../images/thumbnails/flesh-pic.jpg';
+import AiGif from '../../images/thumbnails/chris-brokaw.jpg';
 
 import VinylRecord from './VinylRecord';
 
@@ -49,7 +49,7 @@ export default function VinylRecordContainer(): React.ReactElement | null {
       return;
     }
 
-    navigate('/reviews/strange-flesh-croydon-fox');
+    navigate('/features/chris-brokaw-interview');
   };
 
   if (isTablet && !isHome) {
