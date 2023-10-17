93884ca645a796f9f9568d5850b57437f9818ac7
Justin Ma <justin.ma@verkada.com>
2023-10-16 23:44:13 -0400

    support video types

---
+++ src/custom.d.ts
@@ -0,0 +1,29 @@
+declare module '*.mov' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.mp4' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.m4v' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.wmv' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.avi' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.flv' {
+  const src: string;
+  export default src;
+}
